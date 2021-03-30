903a2382dd68b65385746aa706f8e121aeb9dc49
sxidsvit <s.p.antonyuk@gmail.com>
2021-03-30 23:30:14 +0300

    HeaderBar

---
+++ components/HeaderBar.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { TouchableOpacity, Text, View, Image } from 'react-native'
+import { COLORS, SIZES, FONTS, icons } from '../constants'
+
+const HeaderBar = ({ title, leftOnPressed, right, containerStyle }) => {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        paddingHorizontal: SIZES.padding,
+        ...containerStyle
+      }}
+    >
+      {/* Back */}
+      <View style={{
+        alignItems: 'flex-start'
+      }}>
+        <TouchableOpacity style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: 50,
+          width: 50,
+          borderRadius: 25,
+          backgroundColor: COLORS.transparentBlack
+        }}
+          onPress={leftOnPressed}
+        >
+          <Image
+            source={icons.left_arrow}
+            resizeMode='contain'
+            style={{
+              width: 20,
+              height: 20,
+              tintColor: COLORS.white
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+      {/* Title */}
+      <View style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <Text style={{
+          color: COLORS.white, ...FONTS.h3
+        }}>
+          {title}
+        </Text>
+      </View>
+      {/* Settings */}
+      <TouchableOpacity
+        style={{
+          width: 50,
+          height: 50,
+          borderRadius: 25,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: right ? COLORS.transparentBlack : null
+        }}>
+        {right &&
+          <Image
+            source={icons.settings}
+            resizeMode='contain'
+            style={{
+              width: 20,
+              height: 20,
+              tintColor: COLORS.white
+            }}
+          />
+        }
+
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default HeaderBar
--- screens/Place.js
+++ screens/Place.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     View,
-    Text
+    Text,
+    ImageBackground,
+    Image,
+    Animated
 } from 'react-native';
 
-const Place = () => {
+import { HeaderBar } from '../components'
+
+import { SIZES, FONTS, COLORS, icons } from '../constants'
+
+const Place = ({ navigation, route }) => {
+
+    const [selectedPlace, setSelectedPlace] = useState(null)
+
+    useEffect(() => {
+        let { selectedPlace } = route.params
+        setSelectedPlace(selectedPlace)
+    }, [])
+
+
+    const renderPlace = () => {
+        return (
+            <ImageBackground
+                source={selectedPlace?.image}
+                style={{
+                    width: '100%',
+                    height: '100%'
+                }}
+            >
+                <HeaderBar
+                    title=''
+                    leftOnPressed={() => navigation.goBack()}
+                    right={false}
+                    containerStyle={{
+                        marginTop: SIZES.padding * 2
+                    }}
+                />
+
+            </ImageBackground>
+
+        )
+    }
+
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Place</Text>
+            {renderPlace()}
         </View>
     )
 }
